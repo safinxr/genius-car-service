d81f3867ecb9406bbfbb356a67f9eb556b525ecb
Safinxr <safinxr@gmail.com>
2022-04-13 06:11:18 +0600

    navbar edit

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import logo from '../../../images/logo.png'
+import React from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import logo from "../../../images/logo.png";
 
 const Header = () => {
-    return (
-        <>
-            <Navbar bg="primary" variant="dark">
-                <Container>
-                    <Navbar.Brand href="#home">
-                        <img src={logo} height="30" alt="" />
-                    </Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-
-        </>
-    );
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="primary" sticky='top' variant="dark">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img height={30} src={logo} alt="" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link as={Link} to="/about">
+              About
+            </Nav.Link>
+            <Nav.Link eventKey={2} as={Link} to="/login">
+              Login
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
